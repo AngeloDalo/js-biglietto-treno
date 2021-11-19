9ebe3cac3864a119370d0f68dc8053602349aded
AngeloDalo <angelo251158@gmail.com>
2021-11-19 15:57:26 +0100

    no console.log

--- js/script.js
+++ js/script.js
@@ -14,25 +14,25 @@ let etaUtente = parseInt(prompt('Inserisci età: '));
 while ((etaUtente%1!=0) || (etaUtente<0)) {
     etaUtente = parseInt(prompt('Inserisci età: '));
 }
-console.log("Etò: " + etaUtente);
+//console.log("Etò: " + etaUtente);
 
 //inserisci numero di km valido solo se maggiore di 0 ed è un numero
 let numeroKm = parseInt(prompt('Inserisci numero chilometri : '));
 while ((numeroKm<0) || (numeroKm%1!=0)) {
     numeroKm = parseInt(prompt('Inserisci numero chilometri : '));
 }
-console.log("Numero km: " + numeroKm);
+//console.log("Numero km: " + numeroKm);
 
 //calcolo prezzo biglietto
 if ((etaUtente > 17) && (etaUtente < 65)) {
     prezzoBiglietto = numeroKm * prezzoKm;
-    console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
+    //console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
 } else if (etaUtente < 18) {
     prezzoBiglietto = (numeroKm * prezzoKm) - (((numeroKm * prezzoKm) / 100) * 20);
-    console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
+    //console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
 } else {
     prezzoBiglietto = (numeroKm * prezzoKm) - (((numeroKm * prezzoKm) / 100) * 40);
-    console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
+    //console.log("Prezzo biglietto: " + prezzoBiglietto.toFixed(2) + "€");
 }
 
 //risultato in html
